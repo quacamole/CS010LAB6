fcd908f2d251a155db14316540eda7b06ccc367b
quacamole <102833311+quacamole@users.noreply.github.com>
2022-05-04 13:46:26 -0700

    Initial commit

---
+++ arithmeticExpression.cpp
@@ -0,0 +1,113 @@
+//
+// Created by keraw on 5/4/2022.
+//
+
+#include "arithmeticExpression.h"
+#include<iostream>
+#include<sstream>
+using namespace std;
+
+//added constructor -Kera
+arithmeticExpression::arithmeticExpression(const string& _infix)
+{
+    infixExpression = _infix;
+    root = nullptr;
+}
+
+//optional destructor maybe later
+void arithmeticExpression::buildTree()
+{
+
+}
+void arithmeticExpression::infix(){
+
+}
+void arithmeticExpression::prefix(){
+
+}
+void arithmeticExpression::postfix(){
+
+}
+
+int arithmeticExpression::priority(char op){
+    int priority = 0;
+    if(op == '('){
+        priority =  3;
+    }
+    else if(op == '*' || op == '/'){
+        priority = 2;
+    }
+    else if(op == '+' || op == '-'){
+        priority = 1;
+    }
+    return priority;
+}
+
+string arithmeticExpression::infix_to_postfix(){
+    stack<char> s;
+    ostringstream oss;
+    char c;
+    for(unsigned i = 0; i< infixExpression.size();++i){
+        c = infixExpression.at(i);
+        if(c == ' '){
+            continue;
+        }
+        if(c == '+' || c == '-' || c == '*' || c == '/' || c == '(' || c == ')'){ //c is an operator
+            if( c == '('){
+                s.push(c);
+            }
+            else if(c == ')'){
+                while(s.top() != '('){
+                    oss << s.top();
+                    s.pop();
+                }
+                s.pop();
+            }
+            else{
+                while(!s.empty() && priority(c) <= priority(s.top())){
+                    if(s.top() == '('){
+                        break;
+                    }
+                    oss << s.top();
+                    s.pop();
+                }
+                s.push(c);
+            }
+        }
+        else{ //c is an operand
+            oss << c;
+        }
+    }
+    while(!s.empty()){
+        oss << s.top();
+        s.pop();
+    }
+    return oss.str();
+}
+
+//helper function versions of in, pre and postfix (performs preorder traversal)
+void arithmeticExpression::infix(TreeNode *){
+
+}
+void arithmeticExpression::prefix(TreeNode *){
+
+}
+void arithmeticExpression::postfix(TreeNode *){
+
+}
+
+//outFS is causing an error and I forgot why. probably just a library
+void arithmeticExpression::visualizeTree(const string &outputFilename){
+    ofstream outFS(outputFilename.c_str());
+    if(!outFS.is_open()){
+        cout<<"Error opening "<< outputFilename<<endl;
+        return;
+    }
+    outFS<<"digraph G {"<<endl;
+    visualizeTree(outFS,root);
+    outFS<<"}";
+    outFS.close();
+    string jpgFilename = outputFilename.substr(0,outputFilename.size()-4)+".jpg";
+    string command = "dot -Tjpg " + outputFilename + " -o " + jpgFilename;
+    system(command.c_str());
+}
