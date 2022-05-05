5aad7ababd40e2307282400da5a4df9a6103fc3f
btruj007 <102835844+btruj007@users.noreply.github.com>
2022-05-04 22:33:07 -0700

    Update arithmeticExpression.cpp
    *Added <fstream> for visualizeTree function, 
    *Added helper for visualizeTree function (commented, unfinished), 
    *Completed infix function/helper function (untested),
    *Added spacing in some parts of code for visual appeal (done automatically by my ide).

--- arithmeticExpression.cpp
+++ arithmeticExpression.cpp
@@ -1,7 +1,3 @@
-//
-// Created by keraw on 5/4/2022.
-//
-
 #include "arithmeticExpression.h"
 #include<iostream>
 #include <fstream>
@@ -20,53 +16,62 @@ void arithmeticExpression::buildTree()
 {
 
 }
-void arithmeticExpression::infix(){
-
-}
-void arithmeticExpression::prefix(){
-
-}
-void arithmeticExpression::postfix(){
-
-}
-
-int arithmeticExpression::priority(char op){
-    int priority = 0;
-    if(op == '('){
-        priority =  3;
+void arithmeticExpression::infix() {
+    if (root != nullptr)
+    {
+        infix(root);
     }
-    else if(op == '*' || op == '/'){
+}
+void arithmeticExpression::prefix() {
+    if (root != nullptr)
+    {
+        prefix(root);
+    }
+}
+void arithmeticExpression::postfix() {
+    if (root != nullptr)
+    {
+        postfix(root);
+    }
+}
+
+int arithmeticExpression::priority(char op) {
+    int priority = 0;
+    if (op == '(') {
+        priority = 3;
+    }
+    else if (op == '*' || op == '/') {
         priority = 2;
     }
-    else if(op == '+' || op == '-'){
+    else if (op == '+' || op == '-') {
         priority = 1;
     }
     return priority;
 }
 
-string arithmeticExpression::infix_to_postfix(){
+string arithmeticExpression::infix_to_postfix() {
     stack<char> s;
     ostringstream oss;
     char c;
-    for(unsigned i = 0; i< infixExpression.size();++i){
+    for (unsigned i = 0; i < infixExpression.size(); ++i) {
         c = infixExpression.at(i);
-        if(c == ' '){
+        if (c == ' ') {
             continue;
         }
-        if(c == '+' || c == '-' || c == '*' || c == '/' || c == '(' || c == ')'){ //c is an operator
-            if( c == '('){
+        if (c == '+' || c == '-' || c == '*' || c == '/' || c == '(' || c == ')') { //c is an operator
+            if (c == '(') {
                 s.push(c);
             }
-            else if(c == ')'){
-                while(s.top() != '('){
+            else if (c == ')') {
+                while (s.top() != '(') {
                     oss << s.top();
                     s.pop();
                 }
                 s.pop();
             }
-            else{
-                while(!s.empty() && priority(c) <= priority(s.top())){
-                    if(s.top() == '('){
+            else {
+                while (!s.empty() && priority(c) <= priority(s.top())) {
+                    if (s.top() == '(') {
                         break;
                     }
                     oss << s.top();
@@ -75,40 +80,69 @@ string arithmeticExpression::infix_to_postfix(){
                 s.push(c);
             }
         }
-        else{ //c is an operand
+        else { //c is an operand
             oss << c;
         }
     }
-    while(!s.empty()){
+    while (!s.empty()) {
         oss << s.top();
         s.pop();
     }
     return oss.str();
 }
 
-//helper function versions of in, pre and postfix (performs preorder traversal)
-void arithmeticExpression::infix(TreeNode *){
-
-}
-void arithmeticExpression::prefix(TreeNode *){
-
-}
-void arithmeticExpression::postfix(TreeNode *){
-
-}
-
-//outFS is causing an error and I forgot why. probably just a library
-void arithmeticExpression::visualizeTree(const string &outputFilename){
-    ofstream outFS(outputFilename.c_str());
-    if(!outFS.is_open()){
-        cout<<"Error opening "<< outputFilename<<endl;
+//helper function versions of in, pre and postfix (performs preorder traversal) [Left, Root, Right]
+void arithmeticExpression::infix(TreeNode* _root) {
+    //Prints Left
+    if (_root->left != nullptr)
+    {
+        cout << "(";
+        infix(_root->left);
+         
+    }
+    else
+    {
+        cout << _root->data;
         return;
     }
-    outFS<<"digraph G {"<<endl;
-    visualizeTree(outFS,root);
-    outFS<<"}";
+
+    //Prints Root
+    cout << _root->data;
+
+    //Prints Right
+    if (_root->right != nullptr)
+    {
+        infix(_root->right);
+        cout << ")";
+    }
+
+    return;
+}
+//[Root, Left, Right]
+void arithmeticExpression::prefix(TreeNode*) {
+
+}
+//[Left, Right, Root]
+void arithmeticExpression::postfix(TreeNode*) {
+
+}
+
+void arithmeticExpression::visualizeTree(const string& outputFilename) {
+    ofstream outFS(outputFilename.c_str());
+    if (!outFS.is_open()) {
+        cout << "Error opening " << outputFilename << endl;
+        return;
+    }
+    outFS << "digraph G {" << endl;
+    visualizeTree(outFS, root);
+    outFS << "}";
     outFS.close();
-    string jpgFilename = outputFilename.substr(0,outputFilename.size()-4)+".jpg";
+    string jpgFilename = outputFilename.substr(0, outputFilename.size() - 4) + ".jpg";
     string command = "dot -Tjpg " + outputFilename + " -o " + jpgFilename;
     system(command.c_str());
 }
+
+//helper for visualizeTree(string)
+//void arithmeticExpression::visualizeTree(ofstream&, TreeNode*){
+//
+//}
